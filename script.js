aa2c71b595b25b2e812001c62a45218d189dc01f
CamiloACarvalho <camilo.carvalho@engenharia.ufjf.br>
2023-08-28 19:38:28 -0300

    terminei o desafio

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-//const show = document.getElementById('password');
-const button = document.getElementById('buttonStyle');
-const inputElement = document.getElementById('input');
 
-const randomMakerPassword = (caracteresNumber) => {
+const button = document.getElementById('buttonStyle');
+const inputElement = document.getElementById('inner');
+const spanElement = document.getElementById('password');
+
+const randomMakerPassword = (number) => {
     const caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!?.,@#$%&*()_-=+:;/|";
     let password = "";
-    for (let index = 0; index < caracteresNumber; index += 1) {
+    for (let index = 0; index < number; index += 1) {
         const randomIndex = Math.floor(Math.random() * caracteres.length);
         password += caracteres.charAt(randomIndex);
     }
-    return console.log(password); //Não sei como atribuir a saída para ser exibida na tela.
+    return password;
 }
-const caracteresNumber = parseInt(inputElement.value);
-button.onclick = randomMakerPassword(caracteresNumber)
 
+button.addEventListener('click', () => {
+    const number = parseInt(inputElement.value);
+    const generatedPassword = randomMakerPassword(number);
+    spanElement.textContent = generatedPassword;
+});
